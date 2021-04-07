c17de490aefa7075263f26d030ecc0ff2defab46
Gustavo Riquena <gustavo_riquena1@outlook.com>
2021-04-07 20:44:44 -0300

    getting auxilio moradia value

--- rpa.deputados.extractor/src/main/java/rpa/deputados/extractor/controller/CamaraController.java
+++ rpa.deputados.extractor/src/main/java/rpa/deputados/extractor/controller/CamaraController.java
@@ -1,5 +1,35 @@
 package rpa.deputados.extractor.controller;
 
+import org.apache.log4j.Logger;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
 public class CamaraController {
+	
+	private static Logger log = Logger.getLogger(CamaraController.class);
+	
+	public void initFlow() {
+		
+		
+		try {
+			Document document = Jsoup.connect("https://www.camara.leg.br/transparencia/gastos-parlamentares")
+					.data("legislatura", "56")
+					.data("ano", "2021")
+					.data("mes", "JAN")
+					.data("por", "deputado")
+					.data("deputado", "141372")
+					.get();
+			
+			Elements tableAuxilio = document.select("div[id='tabela-em-dinheiro-modal'] [id='conteudo-tabela-comparativa'] tbody td ~ td ~ td ~ td");
+			
+			String valorAuxilioMoradia = tableAuxilio.get(0).toString();
+			
+			log.info(valorAuxilioMoradia.replaceAll("\\D", ""));
+			
+		} catch (Exception e) {
+			log.error(e.getMessage());
+		}
+	}
 
 }
--- rpa.deputados.extractor/src/main/java/rpa/deputados/extractor/main/Main.java
+++ rpa.deputados.extractor/src/main/java/rpa/deputados/extractor/main/Main.java
@@ -3,6 +3,8 @@ package rpa.deputados.extractor.main;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
+import rpa.deputados.extractor.controller.CamaraController;
+
 public class Main {
 
 	private static Logger log = Logger.getLogger(Main.class);
@@ -10,6 +12,11 @@ public class Main {
 	public static void main(String[] args) {
 		PropertyConfigurator.configure("resources\\log4j.properties");
 		
-		log.info("Hello world");
+		log.info("Starting flow");
+		
+		CamaraController camaraController = new CamaraController();
+		camaraController.initFlow();
+		
+		log.info("Process ended");
 	}
 }
